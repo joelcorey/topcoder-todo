45ea43231563d3f6f8d6ec4bdfb2c3ff1f143cdf
joel <joeloregonmines@gmail.com>
2018-09-16 08:58:24 -0700

    Remove un-needed object property

--- app-data/app/Http/Controllers/Auth/LoginController.php
+++ app-data/app/Http/Controllers/Auth/LoginController.php
@@ -63,7 +63,6 @@ class LoginController extends Controller
         $user = new User;
         $user->name = $gitHub->getName();
         $user->email = $gitHub->getEmail();
-        $user->password = $gitHub->getToken();
         $user->save();
 
         print_r($user);
